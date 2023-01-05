5864ec951a65535303ea63e54513f48d2bc032b3
Maya <mayajwong8@gmail.com>
2023-01-05 08:14:49 -0500

    created project

---
+++ BreakTimer/BreakTimer/BreakTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakTimerApp.swift
+//  BreakTimer
+//
+//  Created by Maya Wong on 2023-01-05.
+//
+
+import SwiftUI
+
+@main
+struct BreakTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
