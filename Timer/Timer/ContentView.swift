b4de0867f9344559c8b83a0db711bcb69dbdcfde
Maya <mayajwong8@gmail.com>
2023-01-23 19:07:04 -0500

    created new project

---
+++ Timer/Timer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Timer
+//
+//  Created by Maya Wong on 2023-01-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
